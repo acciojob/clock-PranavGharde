38fbc652336116ef2c802f85293f089a595b802a
PranavGharde <pranavgharde32@gmail.com>
2025-05-26 16:30:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+function updateTime() {
+  const timerElement = document.getElementById("timer");
+  const now = new Date();
+  const formattedTime = now.toLocaleString();
+  timerElement.textContent = formattedTime;
+}
+updateTime();
+
+setInterval(updateTime, 1000);
